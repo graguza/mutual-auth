52661167f354bd652a9510549285323baffaa233
Raguza <grzegorzx.raguza@intel.com>
2020-04-08 21:55:26 +0200

    Initial commit

---
+++ HttpCoreClient/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace MyCoreClient
+{
+    using System;
+    using System.Net.Http;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        private const string CertificatePath = "MyClient.pfx";
+        private const string CertificatePassword = "password";
+        private const string Host = "https://localhost:54000";
+
+        public static async Task Main(string[] args)
+        {
+            //reads certificate from file
+            var certificate = new X509Certificate2(CertificatePath, CertificatePassword);
+            var handler = new HttpClientHandler
+            {
+                //this callback validates server certificate
+                //server certificate should be issued by trusted issuer
+                ServerCertificateCustomValidationCallback = (sender, cert, chain, errors) => errors == System.Net.Security.SslPolicyErrors.None,
+            };
+            handler.ClientCertificates.Add(certificate);
+            using var client = new HttpClient(handler);
+
+            try
+            {
+                byte[] messsage = Encoding.UTF8.GetBytes("Hello from the client.<EOF>");
+                // Send hello message to the server. 
+
+                Console.WriteLine("sending");
+                using var httpRequest = new HttpRequestMessage(HttpMethod.Post, Host)
+                {
+                    Content = new ByteArrayContent(messsage)
+                };
+
+                using var response = await client.SendAsync(httpRequest);
+                var message = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TCPHttpServer/Program.cs
@@ -0,0 +1,156 @@
+﻿namespace MutualSslDemo.Server
+{
+    using System;
+    using System.Net;
+    using System.Net.Security;
+    using System.Net.Sockets;
+    using System.Security.Authentication;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Text;
+
+    class Program
+    {
+        private const int Port = 54000;
+        private const string CertificatePath = "MyServer.pfx";
+        private const string CertificatePassword = "password";
+
+        static void Main(string[] args)
+        {
+            var certificate = new X509Certificate2(CertificatePath, CertificatePassword);
+            ServicePointManager.ServerCertificateValidationCallback += new RemoteCertificateValidationCallback(OnRemoteCertificateValidationCallback);
+            SslTcpServer.RunServer(Port, certificate);
+        }
+
+        static bool OnRemoteCertificateValidationCallback(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            return (sslPolicyErrors == SslPolicyErrors.None);
+        }
+
+        public sealed class SslTcpServer
+        {
+            // The certificate parameter specifies the name of the file 
+            // containing the machine certificate.
+            public static void RunServer(int serverPort, X509Certificate2 certificate)
+            {
+                // Create a TCP/IP (IPv4) socket and listen for incoming connections.
+                var listener = new TcpListener(IPAddress.Any, serverPort);
+                listener.Start();
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a client...");
+                    var client = listener.AcceptTcpClient();
+                    ProcessClient(client, certificate);
+                }
+            }
+
+            static void ProcessClient(TcpClient client, X509Certificate certificate)
+            {
+                // SslStream using the client's stream.
+                var sslStream = new SslStream(client.GetStream(), false);
+
+                try
+                {
+                    // Authenticate the server and requires the client to authenticate.
+                    sslStream.AuthenticateAsServer(certificate, true, SslProtocols.Default, true);
+                    LogCertificateDetails(sslStream);
+                    sslStream.ReadTimeout = 5000;
+                    sslStream.WriteTimeout = 5000;
+                    Console.WriteLine("Waiting for client message...");
+                    string messageData = ReadMessage(sslStream);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Received: {0}", messageData);
+                    Console.ResetColor();
+
+                    var message = GetMessage();
+                    Console.WriteLine("Sending hello message.");
+
+                    sslStream.Write(message);
+                    sslStream.Flush();
+                }
+                catch (AuthenticationException e)
+                {
+                    Console.WriteLine("Exception: {0}", e.Message);
+                    if (e.InnerException != null)
+                    {
+                        Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+                    }
+                    Console.WriteLine("Authentication failed - closing the connection.");
+                    sslStream.Close();
+                    client.Close();
+                    return;
+                }
+                finally
+                {
+                    // The client stream will be closed with the sslStream
+                    // because we specified this behavior when creating
+                    // the sslStream.
+                    sslStream.Close();
+                    client.Close();
+                }
+            }
+
+            private static byte[] GetMessage()
+            {
+                //we create message that looks like http response
+                var sb = new StringBuilder();
+                sb.AppendLine("HTTP/1.1 200 OK");
+                sb.AppendLine("Host: localhost: 54000");
+                sb.AppendLine("Content-Length: 27");
+                sb.AppendLine();
+                sb.AppendLine("Hello from the server.<EOF>");
+
+                byte[] message = Encoding.UTF8.GetBytes(sb.ToString());
+                return message;
+            }
+
+            static string ReadMessage(SslStream sslStream)
+            {
+                // Read the  message sent by the client.
+                // The client signals the end of the message using the
+                // "<EOF>" marker.
+                byte[] buffer = new byte[2048];
+                StringBuilder messageData = new StringBuilder();
+                int bytes = -1;
+                do
+                {
+                    // Read the client's message.
+                    bytes = sslStream.Read(buffer, 0, buffer.Length);
+                    var decoder = Encoding.UTF8.GetDecoder();
+                    char[] chars = new char[decoder.GetCharCount(buffer, 0, bytes)];
+                    decoder.GetChars(buffer, 0, bytes, chars, 0);
+                    messageData.Append(chars);
+                    // Check for EOF or an empty message.
+                    if (messageData.ToString().IndexOf("<EOF>") != -1)
+                    {
+                        break;
+                    }
+                } while (bytes != 0);
+
+                return messageData.ToString();
+            }
+
+            static void LogCertificateDetails(SslStream stream)
+            {
+                if (stream.LocalCertificate != null)
+                {
+                    Console.WriteLine("LocalCertificate subject {0} valid from {1} to {2}.",
+                        stream.LocalCertificate.Subject,
+                        stream.LocalCertificate.GetEffectiveDateString(),
+                        stream.LocalCertificate.GetExpirationDateString());
+                }
+
+                if (stream.RemoteCertificate == null)
+                {
+                    return;
+                }
+
+                Console.WriteLine("RemoteCertificate subject {0} valid from {1} to {2}.",
+                      stream.RemoteCertificate.Subject,
+                      stream.RemoteCertificate.GetEffectiveDateString(),
+                      stream.RemoteCertificate.GetExpirationDateString());
+
+            }
+        }
+    }
+}
